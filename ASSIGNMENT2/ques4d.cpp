0344102f45d53aa5ba0fbb248c7fe4407436129f
JAYANT KAUSHIK <jkaushik_be24@thapar.edu>
2025-08-13 12:20:25 +0530

    Update ques4d.cpp

--- ASSIGNMENT2/ques4d.cpp
+++ ASSIGNMENT2/ques4d.cpp
@@ -3,48 +3,23 @@ ques-->4d
 (d) Write a program to sort the strings in alphabetical order.
 */
 
-#include <iostream>
-using namespace std;
+int main() {
+    char str[100];
+    cin.getline(str, 100);
 
-bool compareStrings(char a[], char b[]) {
-    int i = 0;
-    while (a[i] != '\0' && b[i] != '\0') {
-        if (a[i] < b[i]) return false;
-        if (a[i] > b[i]) return true;
-        i++;
-    }
-    if (b[i] == '\0' && a[i] != '\0') return true;
-    return false;
-}
+    for (int i = 0; str[i] != '\0'; i++) {
 
-void copyString(char dest[], char src[]) {
-    int i = 0;
-    while (src[i] != '\0') {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-}
+        for (int j = i + 1; str[j] != '\0'; j++) {
 
-void sortStrings(char arr[][100], int n) {
-    char temp[100];
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (compareStrings(arr[i], arr[j])) {
-                copyString(temp, arr[i]);
-                copyString(arr[i], arr[j]);
-                copyString(arr[j], temp);
+            if (str[i] > str[j]) {
+
+                char temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+
             }
         }
     }
-    for (int i = 0; i < n; i++) cout << arr[i] << "\n";
-}
 
-int main() {
-    int n;
-    cin >> n;
-    cin.ignore();
-    char arr[100][100];
-    for (int i = 0; i < n; i++) cin.getline(arr[i], 100);
-    sortStrings(arr, n);
+    cout << str;
 }
