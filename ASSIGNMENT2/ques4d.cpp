7ef63c0de4bd0afe464a7535875db2e509ee4576
JAYANT KAUSHIK <jkaushik_be24@thapar.edu>
2025-08-13 12:24:45 +0530

    Update ques4d.cpp

--- ASSIGNMENT2/ques4d.cpp
+++ ASSIGNMENT2/ques4d.cpp
@@ -3,23 +3,33 @@ ques-->4d
 (d) Write a program to sort the strings in alphabetical order.
 */
 
+#include <iostream>
+using namespace std;
+
+void sortString(char s[]) {
+
+    for (int i = 0; s[i] != '\0'; i++) {
+
+        for (int j = i + 1; s[j] != '\0'; j++) {
+
+
+            if (s[i] > s[j]) {
+                char t = s[i];
+                s[i] = s[j];
+                s[j] = t;
+
+       }
+ }
+}
+}
+    
+
+
 int main() {
     char str[100];
     cin.getline(str, 100);
 
-    for (int i = 0; str[i] != '\0'; i++) {
-
-        for (int j = i + 1; str[j] != '\0'; j++) {
-
-            if (str[i] > str[j]) {
-
-                char temp = str[i];
-                str[i] = str[j];
-                str[j] = temp;
-
-            }
-        }
-    }
-
+    sortString(str);
+    
     cout << str;
 }
